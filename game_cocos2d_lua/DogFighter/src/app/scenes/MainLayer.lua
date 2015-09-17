1e012d36103e24c699e4e9340ee2323767185f09
https://github.com/zengbinsi/game_cocos2d.git <zengbinsi@hotmail.com>
2015-09-17 10:35:38 +0800

    创建背景
    创建主场景背景

--- game_cocos2d_lua/DogFighter/src/app/scenes/MainLayer.lua
+++ game_cocos2d_lua/DogFighter/src/app/scenes/MainLayer.lua
@@ -2,8 +2,16 @@ local M = class("MainLayer", function()
     return cc.Layer:create()
 end)
 
+function M:ctor(parameters)
+	self:onRender()
+end
 
-
+function M:onRender()
+	self.bg = cc.Sprite:create("img/bkg/bg_02.jpg")
+	self.bg:setAnchorPoint(cc.p(1, 1))
+	self.bg:setPosition(V.w_2, V.h_2)
+	self:addChild(self.bg)
+end
 
 
 
